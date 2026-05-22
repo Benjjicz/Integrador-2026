f9ecacc197ff194241b833f3a844f24fe8938919
suldsma <151953663+suldsma@users.noreply.github.com>
2026-05-22 19:57:48 -0300

    Limpiar rama main: dejo el repositorio vacío para empezar de cero

--- frontend/src/app/features/clientes/clientes.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-clientes',
-  imports: [],
-  templateUrl: './clientes.html',
-  styleUrl: './clientes.scss',
-})
-export class Clientes {}
